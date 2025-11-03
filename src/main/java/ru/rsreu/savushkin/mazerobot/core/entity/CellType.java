14a648f4cd5254877b40ca1887cb8354e7fbc03e
Daniel Savushkin <savushckindaniil@yandex.ru>
2025-11-03 23:06:09 +0300

    refactor(core): refactor CellType, Point

--- src/main/java/ru/rsreu/savushkin/mazerobot/core/entity/CellType.java
+++ src/main/java/ru/rsreu/savushkin/mazerobot/core/entity/CellType.java
@@ -1,9 +1,3 @@
 package ru.rsreu.savushkin.mazerobot.core.entity;
 
-public enum CellType {
-    EMPTY,      // Пустая клетка
-    WALL,       // Стена
-    ROBOT,      // Робот
-    TREASURE,   // Клад
-    VISITED     // Посещенная клетка (для пути)
-}
+public enum CellType { EMPTY, WALL, ROBOT, TREASURE, VISITED }
